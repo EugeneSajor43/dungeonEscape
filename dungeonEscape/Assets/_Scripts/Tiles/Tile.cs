84caf0715b93ee1f0ef61e3f7d3dbd59160d3b57
Eugene Sajor <45584408+EugeneSajor43@users.noreply.github.com>
2024-04-26 23:01:40 -0400

    dungeonEscape.unitypackage
    dungeonEscape.unitypackage

---
+++ dungeonEscape/Assets/_Scripts/Tiles/Tile.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Tile : MonoBehaviour {
+    public string TileName;
+    [SerializeField] protected SpriteRenderer _renderer;
+    [SerializeField] private GameObject _highlight;
+    [SerializeField] private bool _isWalkable;
+
+    public BaseUnit OccupiedUnit;
+    public bool Walkable => _isWalkable && OccupiedUnit == null;
+
+
+    public virtual void Init(int x, int y)
+    {
+      
+    }
+
+    void OnMouseEnter()
+    {
+        _highlight.SetActive(true);
+        MenuManager.Instance.ShowTileInfo(this);
+    }
+
+    void OnMouseExit()
+    {
+        _highlight.SetActive(false);
+        MenuManager.Instance.ShowTileInfo(null);
+    }
+
+    void OnMouseDown() {
+        UnitManager.Instance.PlayerMove();
+    }
+
+    public void SetUnit(BaseUnit unit) {
+        if (unit.OccupiedTile != null) unit.OccupiedTile.OccupiedUnit = null;
+        unit.transform.position = transform.position;
+        OccupiedUnit = unit;
+        unit.OccupiedTile = this;
+    }
+}
